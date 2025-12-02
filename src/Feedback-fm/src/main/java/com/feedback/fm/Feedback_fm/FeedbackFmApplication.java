218896fcd899fd7293cade699f1fabd94f2b41a5
OmarH <omarhammad75@gmail.com>
2025-12-02 16:26:45 -0600

    Created Spring App

---
+++ src/Feedback-fm/src/main/java/com/feedback/fm/Feedback_fm/FeedbackFmApplication.java
@@ -0,0 +1,14 @@
+package com.feedback.fm.Feedback_fm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackFmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackFmApplication.class, args);
+	}
+
+
+}
