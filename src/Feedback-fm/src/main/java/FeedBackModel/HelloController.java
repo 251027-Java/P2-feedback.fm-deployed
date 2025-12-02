fc8c9f5a7fdc7b8a11c875e69e471c6e19290051
OmarH <omarhammad75@gmail.com>
2025-12-02 16:59:02 -0600

    keep changes

--- src/Feedback-fm/src/main/java/FeedBackModel/HelloController.java
+++ src/Feedback-fm/src/main/java/FeedBackModel/HelloController.java
@@ -2,13 +2,3 @@ package feedbackmodel;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController; 
-
-
-
-
-@RestController
-public class HelloController {
-    @GetMapping("/hello")
-    public String;
-    }
-}
