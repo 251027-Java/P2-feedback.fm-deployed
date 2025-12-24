530d723717cdc96b1e8432d1f4f0845d2219b833
mlineselle <mlineselle@gmail.com>
2025-12-24 11:46:07 -0600

    Register discovery clients

--- backend/music-metadata-service/src/main/java/com/feedback/musicmetadata/MetadataApplication.java
+++ backend/music-metadata-service/src/main/java/com/feedback/musicmetadata/MetadataApplication.java
@@ -2,8 +2,10 @@ package com.feedback.musicmetadata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MetadataApplication {
     public static void main(String[] args) {
         SpringApplication.run(MetadataApplication.class, args);
